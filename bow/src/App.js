71d372cb3381aa1b4a3e6d6a52123a94a4cb9899
Michael Covarrubias <mdcovarrubias@gmail.com>
2020-10-21 16:41:19 -0700

    remove unnecessary styling.

--- bow/src/App.js
+++ bow/src/App.js
@@ -6,16 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          In Deep Ship
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
