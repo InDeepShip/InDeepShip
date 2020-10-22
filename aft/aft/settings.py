a245529a716644741ea2398b2eef15e1713999ef
Michael Covarrubias <mdcovarrubias@gmail.com>
2020-10-22 13:17:19 -0700

    Add Djongo package to connect to mongoDB

--- aft/aft/settings.py
+++ aft/aft/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'aft.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'djongo',
+        'NAME': 'InDeepShipData',
+        'host': 'localhost',
+        'port': '27017',
     }
 }
 
