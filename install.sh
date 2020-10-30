ad54d1d19ba99f179bc10e9e0245364243526881
Michael Covarrubias <mdcovarrubias@gmail.com>
2020-10-30 10:35:49 -0700

    Adding packages for deployment

--- install.sh
+++ install.sh
@@ -14,5 +14,26 @@ fi
 sudo apt update
 sudo apt install python3-pip python3-dev nginx curl
 
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+source ~/.bashrc
+nvm install v12.19.0
 
 git clone git@github.com:InDeepShip/InDeepShip.git
+pushd InDeepShip
+git checkout production
+
+########################################
+# 1. Need to build Client Code (React Frontend)
+########################################
+pushd bow
+
+npm install
+npm run build
+popd
+
+########################################
+# 2. Need to build Server Code (Django Backend)
+########################################
+python3 -m venv --system-site-packages ./venv
+source ./venv/bin/activate
+pip install -r requirements.txt
