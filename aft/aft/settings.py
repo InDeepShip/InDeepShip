a2b20a5f2a25129057fc4ef0d36da9faf6c0ba0c
Michael Covarrubias <mdcovarrubias@gmail.com>
2020-11-03 11:25:39 -0800

    Add ability to register via rest framework.

--- aft/aft/settings.py
+++ aft/aft/settings.py
@@ -37,13 +37,21 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'drf_yasg',
     'rest_framework',
     'rest_framework.authtoken',
     'rest_auth',
+    'allauth',
+    'allauth.account',
+    'rest_auth.registration',
     'api',
     'users'
 ]
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- aft/api/urls.py
+++ aft/api/urls.py
@@ -2,6 +2,7 @@
 from django.urls import include, path
 
 urlpatterns = [
-    path('users/', include('users.urls')),
-    path('rest-auth/', include('rest_auth.urls'))
+    path('rest-auth/', include('rest_auth.urls')),
+    # path('users/', include('users.urls')),
+    path('rest-auth/registration/', include('rest_auth.registration.urls')),
 ]
