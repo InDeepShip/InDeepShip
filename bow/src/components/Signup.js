574b318e9059d67d4cf42e953592e3c611bf44a6
Maxwell Bradley <maxwell.r.bradley@gmail.com>
2020-11-03 09:28:56 -0800

    Major stylistic changes to the sign up page. Need to work on adding functionality to push credentials to backend server.

--- bow/src/components/Signup.js
+++ bow/src/components/Signup.js
@@ -6,21 +6,28 @@ class Signup extends Component{
         return (
           <div className="container">
             <div className="content">
-                <div className="column is-half">
-                  <figure className="image has-text-centered">
+              <div align="center">
+                <div>
+                  <figure className="image">
                     <img className="is-rounded" src={ourFlag} style={{ height: '384px', width: '384px', display: 'inline-block' }} alt="Our Flag"/>
                   </figure>
               </div>
                 <h1 className="is-size-2">Create account</h1>
-              <div className="is-vcentered">
+                  <div className="field">
+                      <label className="label">Email address</label>
                       <input placeholder="Email address"type="text" name="email"/>
-                  <br/>
+                  </div>
+                  <div className="field">
+                      <label className="label">Password</label>
                       <input placeholder="Password"type="password"name="password1"/>
+                  </div>
+                  <div className="field">
+                      <label className="label">Re-enter password</label>
+                      <input placeholder="Reenter Password"type="password"name="password2"/>
+                  </div>
                   <br/>
-                  <input placeholder="Reenter Password"type="password"name="password2"/>
-                  <br/>
-                  <input type="submit"value="submit"/>
-                <br />
+                  <input type="submit"value="Submit"/>
+                <br/>
             </div>
             </div>
           </div>
