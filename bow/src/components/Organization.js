9c06e40899dd2f1c449d0cc99135596f7b555fce
Michael Covarrubias <mdcovarrubias@gmail.com>
2020-11-04 12:10:00 -0800

    Fix img and p styling to not effect all img and p tags

--- bow/src/components/Organization.js
+++ bow/src/components/Organization.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import brandingImg from '../assets/our_flag.png';
 import '../styles/Organization.scss';
 
 class Organization extends Component {
@@ -10,7 +11,25 @@ class Organization extends Component {
 
     render() {
         return (
-            <section className='hero is-large'></section>
+            <section className='hero is-large'>
+                <div className='hero-body'>
+                    <div className='container'>
+                        <div className='columns is-vcentered'>
+                            <div className='column is-6'>
+                                <h1 className='title'>
+                                    Organization Title
+                                </h1>
+                                <h2 className='sub-title'>
+                                    Our Organzation stands for FREEDOM
+                                </h2>
+                            </div>
+                            <div className='column is-5 has-text-centered'>
+                                <img src={brandingImg} alt="Logo" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         );
     }
 }
