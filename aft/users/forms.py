a71b8558f2c3146e8a3bc5d0e6643b7280288f53
bdhillon <bdhillon1994@gmail.com>
2020-11-04 13:54:45 -0800

    added password reset forms to users app

--- aft/users/forms.py
+++ aft/users/forms.py
@@ -1,6 +1,6 @@
 # users/forms.py
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordResetForm, PasswordChangeForm
 from .models import CustomUser
 
 
@@ -16,3 +16,17 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
         fields = UserChangeForm.Meta.fields
+
+
+class CustomPasswordResetForm(PasswordResetForm):
+
+    class Meta:
+        model = CustomUser
+        fields = PasswordResetForm.Meta.fields
+
+
+class CustomPasswordChangeForm(PasswordChangeForm):
+
+    class Meta:
+        model = CustomUser
+        fields = PasswordChangeForm.Meta.fields
