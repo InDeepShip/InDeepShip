caa36bcde4b0e22e5a280a53ab376792d1bf1f85
bdhillon <bdhillon1994@gmail.com>
2020-11-03 23:42:35 -0800

    added markdown for api viewer, added api_overview and updated bug_report

--- aft/api/urls.py
+++ aft/api/urls.py
@@ -4,5 +4,6 @@ from api import views
 
 urlpatterns = [
     path('users/', include('users.urls')),
-    path('bugreport/', views.BugReportView)
+    path('bugreport/', views.bug_report),
+    path('', views.api_overview)
 ]
