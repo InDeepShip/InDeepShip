81e6549ae07d2a8ddbaa05765cd786e667995c00
bdhillon <bdhillon1994@gmail.com>
2020-11-05 10:21:01 -0800

    change password endpoint  is working

--- aft/users/views.py
+++ aft/users/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from rest_framework import status
 from rest_framework import generics
 from django.dispatch import receiver
 from django.urls import reverse
 from django_rest_passwordreset.signals import reset_password_token_created
 from django.core.mail import send_mail
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from . import models
 from . import serializers
@@ -12,6 +14,41 @@ from . import serializers
 class UserListView(generics.ListAPIView):
     queryset = models.CustomUser.objects.all()
     serializer_class = serializers.UserSerializer
+
+
+class ChangePasswordView(generics.UpdateAPIView):
+    """
+    An endpoint for changing a users password. Accepts `old_password` and `new_password` fields. Returns status.
+    """
+    serializer_class = serializers.ChangePasswordSerializer
+    model = models.CustomUser
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self, queryset=None):
+        obj = self.request.user
+        return obj
+
+    def update(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        serializer = self.get_serializer(data=request.data)
+
+        if serializer.is_valid():
+            # Check old password
+            if not self.object.check_password(serializer.data.get("old_password")):
+                return Response({"old_password": ["Wrong password."]}, status=status.HTTP_400_BAD_REQUEST)
+            # set_password also hashes the password that the user will get
+            self.object.set_password(serializer.data.get("new_password"))
+            self.object.save()
+            response = {
+                'status': 'success',
+                'code': status.HTTP_200_OK,
+                'message': 'Password updated successfully',
+                'data': []
+            }
+
+            return Response(response)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @receiver(reset_password_token_created)
