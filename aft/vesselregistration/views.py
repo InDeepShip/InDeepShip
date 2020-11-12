dbb87ecee725c29c0ece0cacccab66a27ce74a54
Michael Covarrubias <mdcovarrubias@gmail.com>
2020-11-12 13:52:51 -0800

    Add function to handle private registration POST requests

--- aft/vesselregistration/views.py
+++ aft/vesselregistration/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+
 
 # Create your views here.
+@api_view(['POST'])
+def private_register(request):
+    """
+    Method used to handle user private vessel registration
+    """
+    print('was able to get post')
