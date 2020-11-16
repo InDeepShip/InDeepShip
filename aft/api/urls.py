77d2af41c8691c8ba6a6050cd35ae307c4bc0b94
Maxwell Bradley <maxwell.r.bradley@gmail.com>
2020-11-16 08:25:59 -0800

    Created api endpoint to list the current ports in the database.

--- aft/api/urls.py
+++ aft/api/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('users/', include('users.urls')),
     path('bugreport/', views.bug_report),
     path('vessel_lookup/', views.vessel_lookup),
+    path('ports/', views.ports),
     path('', views.api_overview)
 ]
