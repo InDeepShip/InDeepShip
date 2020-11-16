e3d514f6adcf701c7e84d8b491997ec89fbef09b
Maxwell Bradley <maxwell.r.bradley@gmail.com>
2020-11-16 10:40:13 -0800

    Got rendering of port names done on the frontend.

--- bow/src/components/VesselNameLookup.js
+++ bow/src/components/VesselNameLookup.js
@@ -24,104 +24,99 @@ const renderField = ({
 class VesselNameLookup extends Component {
   constructor(props) {
     super(props);
-    let port_names;
-    fetch("http://206.189.218.111/api/vessel_lookup/", {
-      method: 'POST',
-      body: JSON.stringify({
-        "vesselName": vessel,
-        "portName": port
-      }),
+    const vessel = "Vessel";
+    this.state = {
+      ports: [],
+      selectedPort: ""
+    }
+    fetch("http://206.189.218.111/api/ports/", {
+      method: 'GET',
       headers: {
         'Content-Type': 'application/json'
       }
-    }
-    ).then(response) => response.json())
-    .then(data => {
-      this.setState({ "ports": data["ports"] });
-    })
-
-
-  })
-        // this.state = { "portName": "Whitby Harbour" };
-  }
-
-onSubmit = (e) => {
-  const { vesselName, portName } = this.state;
-  const vessel = this.state["vesselName"];
-  const port = this.state["portName"];
-  fetch("http://206.189.218.111/api/vessel_lookup/",
-    {
-      method: 'POST',
-      body: JSON.stringify({
-        "vesselName": vessel,
-        "portName": port
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    }
-  )
-    .then((response) => response.json())
+    }).then((response) => response.json())
     .then(data => {
       this.setState({
-        availability: data["message"]
+        "ports": data["ports"]
+      })
+    })
+  }
+
+  checkNameAvailability = (e) => {
+    const { vesselName, portName } = this.state;
+    const vessel = this.state["vesselName"];
+    const port = this.state["selectedPort"];
+    fetch("http://206.189.218.111/api/vessel_lookup/",
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          "vesselName": vessel,
+          "portName": port
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    )
+      .then((response) => response.json())
+      .then(data => {
+        this.setState({
+          availability: data["message"]
+        }
+        );
       }
       );
-    }
-    );
 
-}
+  }
 
-handleChange = (e) => {
-  this.setState({ [e.target.name]: e.target.value });
-}
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
 
-render() {
-  return (
-    <section className='hero is-large'>
-      <div className='container'>
-        <div className='columns is-vcentered'>
-          <div className='column is-5 has-text-centered'>
-            <img src={brandingImg} alt="Logo" />
-          </div>
-          <div className='column is-6'>
-            <h1 className='title'>
-              Check for Vessel name availability
-                                </h1>
-            <div className="field">
-              <label className="label">Vessel Name</label>
-              <input className='input' placeholder="Vessel name" type="text" name="vesselName" onChange={this.handleChange} />
+  render() {
+    return (
+      <section className='hero is-large'>
+        <div className='container'>
+          <div className='columns is-vcentered'>
+            <div className='column is-5 has-text-centered'>
+              <img src={brandingImg} alt="Logo" />
             </div>
-            <div className='field'>
-              <label className='label'>Desired Home Port</label>
-              <div class="control">
-                <div class="select">
-                  <select value={this.state.portName} onChange={(e) => this.setState({ portName: e.target.value })}>
-                    <option value='Whitby Harbour'>Whitby Harbour</option>
-                    <option value='Scarborough'>Scarborough</option>
-                    <option value='Point Newcastle'>Point Newcastle</option>
-                    <option value='Barrow'>Barrow</option>
-                    <option value='Robin Hood&#39;s Bay'>Robin Hood's Bay</option>
-                    <option value='Victoria'>Victoria</option>
-                  </select>
+            <div className='column is-6'>
+              <h1 className='title'>
+                Check for Vessel name availability
+                                </h1>
+              <div className="field">
+                <label className="label">Vessel Name</label>
+                <input className='input' placeholder="Vessel name" type="text" name="vesselName" onChange={this.handleChange} />
+              </div>
+              <div className='field'>
+                <label className='label'>Desired Home Port</label>
+                <div class="control">
+                  <div class="select">
+                    {/* <select value={this.state.portName} onChange={(e) => this.setState({ portName: e.target.value })}> */}
+                      <select value={this.state.selectedPort} onChange={(e) => this.setState({ selectedPort: e.target.value })}>
+                      {this.state.ports.map((value) => {
+                      return <option key={value} value={value}>{value}</option>
+                    })}
+                    </select>
+                  </div>
                 </div>
               </div>
-            </div>
-            <br />
-            <div className='field'>
-              <div className='control'>
-                <button className='button is-primary' onClick={this.onSubmit}>Check availability</button>
+              <br />
+              <div className='field'>
+                <div className='control'>
+                  <button className='button is-primary' onClick={this.checkNameAvailability}>Check availability</button>
+                </div>
               </div>
-            </div>
-            <div name="resultOfCheck">
-              <h1>{this.state.availability}</h1>
+              <div name="resultOfCheck">
+                <h1>{this.state.availability}</h1>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
-  );
-}
+      </section>
+    );
+  }
 }
 
 export default VesselNameLookup;
