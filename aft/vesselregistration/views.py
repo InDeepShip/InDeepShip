7110d8934b3070f712731879286b645d7bdcf3c0
Michael Covarrubias <mdcovarrubias@gmail.com>
2020-11-16 12:23:19 -0800

    Add csrf exempt in order to reach backend

--- aft/vesselregistration/views.py
+++ aft/vesselregistration/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+@csrf_exempt
 def private_registration(request):
     """
     Function used to handle user private registration
