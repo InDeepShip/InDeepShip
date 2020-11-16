4a0c31fb889676462c5f251f74ec247e2be3f7fa
Michael Covarrubias <mdcovarrubias@gmail.com>
2020-11-16 13:12:54 -0800

    Handle http request to submit a new private registation form

--- aft/vesselregistration/views.py
+++ aft/vesselregistration/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from vesselregistration import models
+import json
 
 # Create your views here.
 @csrf_exempt
@@ -7,4 +10,14 @@ def private_registration(request):
     """
     Function used to handle user private registration
     """
-    print('able to get request')
+    data = json.loads(request.body)
+    data = data['registration']
+
+    try:
+        models.PrivateRegistrationForms.objects.create(**data)
+
+    except Exception as e:
+        print('Issue creating a new private registration')
+        return HttpResponse(status=400)
+
+    return HttpResponse(status=201)
