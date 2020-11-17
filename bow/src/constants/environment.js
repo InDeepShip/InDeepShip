fdd9147be30a1dd8508ede0bc71b53aa3cf68c71
Michael Covarrubias <mdcovarrubias@gmail.com>
2020-11-17 13:00:01 -0800

    Add server address constants
    Currently by default the frontend will make API requests to
    our actual development server.
    
    Instructions are added to get API requests working locally

--- bow/src/constants/environment.js
+++ bow/src/constants/environment.js
@@ -1,2 +1,12 @@
 export const DEVELOPMENT_SERVER_ADDRESS = 'http://127.0.0.1:8000';
 export const DEPLOYMENT_SERVER_ADDRESS = 'http://206.189.218.111';
+
+/*
+    For development locally set:
+    export const SERVER_ADDRESS = DEVELOPMENT_SERVER_ADDRESS
+
+    For deployment set:
+    export const SERVER_ADDRESS = DEPLOYMENT_SERVER_ADDRESS;
+*/
+
+export const SERVER_ADDRESS = DEPLOYMENT_SERVER_ADDRESS;
