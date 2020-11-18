64dbffea6e084fbda753a9c448540baca858d5b6
Siman <wangsiman-carol@outlook.com>
2020-11-17 19:02:06 -0800

    Added private user signup test

--- bow/jest.config.js
+++ bow/jest.config.js
@@ -8,8 +8,22 @@ module.exports = {
   // transform: {
   //   "\\.js$": "<rootDir>/node_modules/babel-jest"
   // },
+
   verbose: true,
-  collectCoverage: false,
-  coverageDirectory: "<rootDir>/coverageReport/",
+  collectCoverage: true,
+  // coveragePathIgnorePatterns: [
+  //   "<rootDir>/node_modules/",
+  //   "<rootDir>/testCoverageReport/",
+  //   "<rootDir>/public/"
+  // ],
+  collectCoverageFrom: [
+    "**/*.{js,jsx}",
+    "!**/node_modules/**",
+    "!**/vendor/**",
+    "!jest.config.js",
+    "!babel.config.js",
+    "!**/testCoverageReport/**"
+  ],
+  coverageDirectory: "<rootDir>/testCoverageReport/",
   preset: "jest-puppeteer"
 };
