426eee7aff2bfbc118b2a5b5e4946967659af8d7
Maxwell Bradley <maxwell.r.bradley@gmail.com>
2020-11-19 11:00:29 -0800

    Added relationships between models in api models file. Now to work on linking the vessels with the registrations.

--- aft/api/models.py
+++ aft/api/models.py
@@ -1,22 +1,23 @@
-from django.db import models
+#from django.db import models
+from djongo import models
 
 # Create your models here.
 
 
 class Port(models.Model):
     name = models.CharField(max_length=128, null=False, default="")
-#    class Meta:
-#        app_label = 'Port'
 
+class Propulsion(models.Model):
+    name = models.CharField(max_length=128, null=False, default="")
 
 class Vessel(models.Model):
     name = models.CharField(max_length=128, null=False, default="")
     port = models.ForeignKey(Port, null=False, on_delete=models.CASCADE)
-#    class Meta:
-#        app_label = 'api.Vessel'
-
-class Propulsion(models.Model):
-    name = models.CharField(max_length=128, null=False, default="")
-#    class Meta:
-#        app_label = 'Propulsion'
+    imo = models.IntegerField(default=0)
+    tonnage = models.DecimalField(max_digits=20, decimal_places=0, default=0)
+    propulsion = models.ForeignKey(Propulsion, null=True, on_delete=models.CASCADE)
+    yard_number = models.DecimalField(max_digits=20, decimal_places=0, default=1)
+    vessel_length = models.DecimalField(max_digits=20, decimal_places=0, default=0) 
+    hulls = models.DecimalField(max_digits=3, decimal_places=0, default=1)
+    purpose = models.CharField(max_length=512, default="")
 
--- aft/vesselregistration/models.py
+++ aft/vesselregistration/models.py
@@ -1,12 +1,10 @@
-from django.db import models
+from djongo import models
+from api.models import Vessel
 
 # Create your models here.
 class Registration(models.Model):
-    name = models.CharField(max_length=255)
     vessel = models.CharField(max_length=255)
-    email = models.EmailField(max_length=255)
-    phone = models.DecimalField(max_digits=11, decimal_places=0)
-    address = models.CharField(max_length=255)
+    #vessel = models.OneToOneField(Vessel, on_delete=models.CASCADE)
     port = models.CharField(max_length=255)
     imo = models.DecimalField(unique=True, max_digits=20, decimal_places=0)
     tonnage = models.CharField(max_length=255)
@@ -18,3 +16,4 @@ class Registration(models.Model):
     vessel_length = models.CharField(max_length=255)
     hulls = models.DecimalField(max_digits=3, decimal_places=0)
     purpose = models.CharField(max_length=512)
+    #def find_vessel(self):
