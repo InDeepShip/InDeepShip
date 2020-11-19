9c00da0638bfd7318b3ffc2b4ddc99b3411ba1d9
bdhillon <bdhillon1994@gmail.com>
2020-11-19 01:51:02 -0800

    updated email template to include query parameters

--- bow/src/constants/routes.js
+++ bow/src/constants/routes.js
@@ -7,6 +7,6 @@ export const LOGIN = '/login';
 export const SIGN_UP = '/signup';
 export const VESSEL_NAME_LOOKUP = '/vesselNameLookup';
 export const PASSWORD_RESET = '/passwordreset'
-export const PASSWORD_RESET_CONFIRM = '/passwordresetconfirm'
+export const PASSWORD_RESET_CONFIRM = '/passwordreset/confirm'
 export const PRIVATE_REGISTRATION_DETAILS = '/private-registration-details';
 export const PRIVATE_REGISTRATION = '/private-registration';
