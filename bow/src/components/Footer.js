15d74d3f67ae3bd9f7b3a6b0b6091300b182f1fc
bdhillon <bdhillon1994@gmail.com>
2020-11-18 22:36:13 -0800

    added social media links in footer

--- bow/src/components/Footer.js
+++ bow/src/components/Footer.js
@@ -17,9 +17,21 @@ class Footer extends Component {
                     <p>
                         <strong>Navis Album DRS</strong> by <a href='https://github.com/InDeepShip'>In Deep Ship Inc</a>.
                     </p>
+                    <span className="social-media-icons">
+                        <a href='https://www.facebook.com/'> <i class="fab fa-facebook-square fa-2x"></i> </a>
+                        &nbsp;
+                        &nbsp;
+                       <a href='https://instagram.com/'> <i class="fab fa-instagram fa-2x"></i></a>
+                        &nbsp;
+                        &nbsp;
+                       <a href='https://twitter.com/'> <i class="fab fa-twitter-square fa-2x"></i></a>
+                        &nbsp;
+                        &nbsp;
+                        <a href='https://linkedin.com/'> <i class="fab fa-linkedin fa-2x"></i></a>
+                    </span>
                 </div>
             </footer>
-       );
+        );
     }
 }
 
