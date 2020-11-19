b8cfa18f00b66cbfdf4386c59c2963d17b9fc45e
bdhillon <bdhillon1994@gmail.com>
2020-11-19 00:33:59 -0800

    moved textbox to footer

--- bow/src/components/Footer.js
+++ bow/src/components/Footer.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
 import '../styles/Footer.scss';
+import axios from 'axios';
+
 
 class Footer extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-
+            bugReportText: ""
         };
+    }
+
+    handleSubmitBugReport() {
+        var message = this.state.bugReportText;
+        var currentPage = window.location;
+
+        axios
+            .post(`${process.env.REACT_APP_SERVER_ADDRESS}/api/bugreport/`, {
+                "message": message,
+                "currentPage": currentPage.href
+            })
+            .then(response => { console.log(response.data) });
+    }
+
+    handleBugReportText(e) {
+        this.setState({
+            bugReportText: e.target.value
+        });
     }
 
     render() {
@@ -29,6 +49,19 @@ class Footer extends Component {
                         &nbsp;
                         <a href='https://linkedin.com/'> <i class="fab fa-linkedin fa-2x"></i></a>
                     </span>
+                    <div className="bugReportForm">
+                        <br /><br />
+                        Please provide a brief description of your feedback:
+                        <br /><br />
+                        <textarea id="bug-report-textbox-selector" align="center" cols="50" rows="10" type='text' onChange={(e) => this.handleBugReportText(e)} />
+                        <br /><br />
+                        <div align="center">
+                            <button id="bug-report-submit-selector" type="button" onClick={() => this.handleSubmitBugReport()} className="button is-warning is-link">Submit</button>
+                        </div>
+                    </div>
+                    {/* scroll a little past the submit button */}
+                        &nbsp;
+                    <div ref={this.props.textboxRef}> </div>
                 </div>
             </footer>
         );
