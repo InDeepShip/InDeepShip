8e22b331e776285b5915ece421b22e1dc95c4dea
Maxwell Bradley <maxwell.r.bradley@gmail.com>
2020-11-19 08:29:28 -0800

    Add private user to database

--- aft/users/models.py
+++ aft/users/models.py
@@ -17,3 +17,9 @@ class Broker(models.Model):
 
     def __str__(self):
         return self.user.email
+
+class PrivateUser(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.email
