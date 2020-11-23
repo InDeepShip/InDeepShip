de0dbda94f9a4f0a9e7a5731107a73ea521692df
Michael Covarrubias <mdcovarrubias@gmail.com>
2020-11-23 12:10:33 -0800

    Remove unneeded layer

--- bow/src/index.js
+++ bow/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import { Provider } from 'react-redux';
-// import Root from './components/Root';
 import store from './store';
 import './styles/index.scss';
 import * as serviceWorker from './serviceWorker';
