2658be27643e5d1911250f083e6d81db51753395
Michael Covarrubias <mdcovarrubias@gmail.com>
2020-11-25 13:01:17 -0800

    Error Submitting payment Page update

--- bow/src/components/ErrorPage.js
+++ bow/src/components/ErrorPage.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import * as ROUTES from '../constants/routes';
+import '../styles/ErrorPage.scss';
 
 class ErrorPage extends Component {
     constructor(props) {
@@ -9,7 +12,25 @@ class ErrorPage extends Component {
 
     render() {
         return (
-            <div>Error submitting payment</div>
+            <section className="hero">
+                <div className="hero-body">
+                    <div className="container">
+                        <div className="columns is-centered">
+                            <div className="column is-4 error-page-column">
+                                <section>
+                                <h1 className="title">Error Submitting Payment</h1>
+                                    <span className="icon has-text-danger">
+                                        <i className="fas fa-3x fa-ban"></i>
+                                    </span>
+                                </section>
+                                <Link as="button" className="button is-primary" to={ROUTES.LANDING}>
+                                    Back Home
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         );
     }
 }
