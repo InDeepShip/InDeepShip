56d5c184be1d4db97a4f1129f5f75424d794b7cc
Michael Covarrubias <mdcovarrubias@gmail.com>
2020-11-25 17:05:30 -0800

    Add ability to create broker account in backend

--- aft/users/urls.py
+++ aft/users/urls.py
@@ -1,13 +1,15 @@
 # users/urls.py
 from django.urls import include, path, re_path
 from rest_auth.views import PasswordResetConfirmView
+from .views import RegisterViewCustom
 
 from . import views
 
 urlpatterns = [
+    path('', include('rest_auth.urls')),
+    path('signup/', RegisterViewCustom.as_view(), name='account_signup'),
     path('signup/', include('rest_auth.registration.urls')),
     path('all', views.UserListView.as_view()),
-    path('', include('rest_auth.urls')),
     re_path(r'^rest-auth/password/reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', PasswordResetConfirmView.as_view(),
             name='password_reset_confirm')
 ]
