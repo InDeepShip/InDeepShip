19055b1cfb89a289fc1956e7fdf185996bc7362f
Abdurrahman Al-Omari <abdurrahman.alomari@gmail.com>
2020-12-14 15:48:00 -0800

    Fixed styling for ship registration

--- bow/src/components/PrivateDashboard.js
+++ bow/src/components/PrivateDashboard.js
@@ -1,53 +1,175 @@
 import React, { Component } from 'react';
 import { withRouter, Link } from 'react-router-dom';
-import { PDFDownloadLink, Page, Text, View, Document, StyleSheet, Image, Font, PDFViewer } from '@react-pdf/renderer';
+import { PDFDownloadLink, Page, Text, View, Document, StyleSheet, Image, Font, PDFViewer, Canvas } from '@react-pdf/renderer';
+// import PDFKit from 'pdfkit';
 import { connect } from 'react-redux';
 import axios from 'axios';
 import '../styles/PrivateDashboard.scss';
 import { VESSEL_NAME_LOOKUP } from '../constants/routes';
+import brandingImg from '../assets/our_flag.png';
+import watermark from "../assets/watermark.png"
+import signature from "../assets/signature.png"
+import coatOfArms from "../assets/Coat_of_arms_of_the_United_Kingdom_(black_and_white).jpg"
 
 const styles = StyleSheet.create({
-  page: {
-    flexDirection: 'row',
-    backgroundColor: '#E4E4E4'
-  },
-  section: {
-    margin: 10,
-    padding: 10,
-    flexGrow: 1
-  },
-  image: {
-    height: 150,
-    marginBottom: 30,
-    marginHorizontal: 100,
-  },
+    page: {
+        // flexDirection: 'row',
+        // backgroundColor: '#FFFFFF'
+        paddingTop: 35,
+        paddingBottom: 65,
+        paddingHorizontal: 35,
+    },
+    title: {
+        fontSize: 16,
+        textAlign: 'left',
+        fontFamily: 'Times-Roman',
+        paddingVertical: 10,
+    },
+    subtitle: {
+        fontSize: 12,
+        textAlign: 'center',
+        fontFamily: 'Times-Roman',
+    },
+    footer: {
+        fontSize: 12,
+        textAlign: 'left',
+        fontFamily: 'Times-Roman',
+    },
+    footer2: {
+        fontSize: 8,
+        textAlign: 'left',
+        fontFamily: 'Times-Roman',
+    },
+    section: {
+        fontSize: 11,
+        marginTop: 12,
+        fontFamily: 'Times-Roman',
+        fontWeight: 'bold',
+        borderBottomColor: '#000000',
+        borderBottomWidth: StyleSheet.hairlineWidth,
+    },
+    text: {
+        marginTop: 10,
+        // padding: 10,
+        paddingRight: 5000,
+        fontSize: 12,
+        textAlign: 'left',
+        fontFamily: 'Times-Roman',
+        letterSpacing: 0.5,
+    },
+    watermark: {
+        position: 'absolute',
+        bottom: 250,
+        right: 0,
+        width: 250,
+        height: 250,
+    },
+    image: {
+        width: 100,
+        height: 50,
+    },
+    image2: {
+        width: 100,
+        height: 30,
+    },
+    signature: {
+        width: 200,
+        height: 50,
+    },
+    pageNumber: {
+        position: 'absolute',
+        fontSize: 12,
+        bottom: 30,
+        left: 0,
+        right: 0,
+        textAlign: 'center',
+        // color: 'grey',
+    },
+});
+
+Font.register({
+    family: 'Oswald',
+    src: 'https://fonts.gstatic.com/s/oswald/v13/Y_TKV6o8WovbUd3m_X9aAA.ttf'
 });
 
 const MyDocument = ({ name, registration }) => (
-  <Document>
-    <Page size="A4" style={styles.page}>
-      <View style={styles.section}>
-        <Text>Name: {name}</Text>
-        <Text>Email: {registration.email}</Text>
-        <Text>Phone: {registration.phone}</Text>
-        <Text>Address: {registration.address}</Text>
-        <Text>Vessel: {registration.vessel}</Text>
-        <Text>Port: {registration.port}</Text>
-        <Text>IMO: {registration.imo}</Text>
-        <Text>Builder: {registration.builder_name}</Text>
-        <Text>Builder Address: {registration.builder_address}</Text>
-        <Text>Yard Number: {registration.yard_number}</Text>
-        <Text>Registration Date: {registration.date}</Text>
-        <Text>Hulls: {registration.hulls}</Text>
-        <Text>Gross Tonnage: {registration.tonnage}</Text>
-        <Text>Method of Propulsion: {registration.propulsion}</Text>
-        <Text>Vessel Length: {registration.vessel_length}</Text>
-      </View>
-      <View style={styles.image}>
-        <Image src="../assets/Coat_of_arms_of_the_United_Kingdom_(black_and_white).svg" />
-      </View>
-    </Page>
-  </Document>
+    <Document>
+        <Page size="A4" style={styles.page}>
+            <View>
+                <Text style={styles.title}><Image style={styles.image2} src={brandingImg} /> CERTIFICATE OF <Image style={styles.image} src={coatOfArms} /> NAVIS ALBUM REGISTRY</Text>
+            </View>
+            <View style={{ marginTop: 10 }}>
+                <Text style={styles.subtitle}>The Merchant Shipping Act 1995</Text>
+                <Text style={styles.subtitle}>The Merchant Shipping (Registration of Ships) Regulations 1993, as amended</Text>
+            </View>
+
+            <View>
+                <Text style={styles.section}>PARTICULARS OF SHIP</Text>
+            </View>
+
+            <View>
+                <Text style={styles.text}>Name of Ship                                         {registration.vessel}</Text>
+                <Text style={styles.text}>Official Number</Text>
+                <Text style={styles.text}>IMO Number / HIN                            {registration.imo}</Text>
+                <Text style={styles.text}>Method of Propulsion                        {registration.propulsion}</Text>
+                <Text style={styles.text}>Engine Make & Model</Text>
+                <Text style={styles.text}>Total Engine Power</Text>
+                <Text style={styles.text}>Length                                                       {registration.vessel_length}  meters</Text>
+                <Text style={styles.text}>Depth</Text>
+                <Text style={styles.text}>Number of Hulls                                   {registration.hulls}</Text>
+                <Text style={styles.text}>Gross Tonnage                                       {registration.tonnage}</Text>
+                <Text style={styles.text}>Registered Tonnage                             {registration.tonnage}</Text>
+                <Text style={styles.text}>Year of Build</Text>
+                <Text style={styles.text}>Name of Builder                                   {registration.builder_name}</Text>
+                <Text style={styles.text}>Country of Build                                  {registration.builder_address}</Text>
+                <Text style={styles.text}>Yard Number                                         {registration.yard_number}</Text>
+                <Text style={styles.text}>Radio Call Sign</Text>
+                <Text style={styles.text}>Port                                                            {registration.port}</Text>
+            </View>
+
+            <View style={styles.section} />
+
+            <View>
+                <Text style={styles.text}>This Certificate was issued on        {registration.date}</Text>
+                <Text style={styles.text}>This Certificate expires on               {registration.expiration_date}</Text>
+                <Text style={styles.text}>Signed <Image style={styles.signature} src={signature} /></Text>
+                <View style={{marginBottom:12}}/>
+                <Text style={styles.footer}>For and on behalf of the Director General, Department of Transport</Text>
+                <View style={{marginBottom:6}}/>
+                <Text style={styles.footer2}>By the Registry of Shipping, an Executive Agency of the Government of Navis Album</Text>
+            </View>
+
+            <View style={styles.watermark}>
+                <Image src={watermark} />
+            </View>
+            <Text style={styles.pageNumber} render={({ pageNumber, totalPages }) => (
+                `${pageNumber} of ${totalPages}`
+            )} fixed />
+        </Page>
+
+        <Page size="A4" style={styles.page}>
+            <View>
+                <Text style={styles.section}>PARTICULARS OF SHIP</Text>
+                <Text style={styles.text}>Name of Ship                                         {registration.vessel}</Text>
+                <Text style={styles.text}>Official Number</Text>
+                <Text style={styles.text}>IMO Number / HIN                             {registration.imo}</Text>
+            </View>
+
+            <View style={{marginBottom:12}}/>
+
+            <View>
+                <Text style={styles.section}>OWNERSHIP DETAILS</Text>
+                <Text style={styles.text}>Name                                                         {name}</Text>
+                <Text style={styles.text}>Email                                                         {registration.email}</Text>
+                <Text style={styles.text}>Phone Number                                      {registration.phone}</Text>
+                <Text style={styles.text}>Address                                                    {registration.address}</Text>
+
+            </View>
+            <Text style={styles.pageNumber} render={({ pageNumber, totalPages }) => (
+                `${pageNumber} of ${totalPages}`
+            )} fixed />
+        </Page>
+    </Document>
 );
 
 
@@ -126,7 +248,7 @@ class PrivateDashboardBase extends Component {
                                             <td>{vessel.status}</td>
                                             <td>
                                                 <PDFDownloadLink className="button is-primary" document={<MyDocument name={name} registration={jsonData} />} fileName="registration.pdf">
-                                                {({ blob, url, loading, error }) => (loading ? 'Download' : 'Download')}
+                                                    {({ blob, url, loading, error }) => (loading ? 'Download' : 'Download')}
                                                 </PDFDownloadLink>
                                             </td>
                                         </tr>
