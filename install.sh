a1ab7bd6ff9a2565d1b02527fbe3e63e5395181e
Michael Covarrubias <mdcovarrubias@gmail.com>
2021-02-02 21:21:59 -0800

    more server changes

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ fi
 
 
 sudo apt update
-sudo  apt install -y python3-pip python3-dev nginx curl
+sudo  apt install -y python3-pip python3-dev nginx curl python3-venv
 
 curl -sL https://deb.nodesource.com/setup_12.x | bash -
 sudo  apt-get install -y nodejs
